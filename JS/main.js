cc18322e774aef67c0d7028ca312f326cc6c676c
femaffi13 <fedemaffi13@gmail.com>
2021-02-13 22:20:24 -0300

    bg/if-else

--- JS/main.js
+++ JS/main.js
@@ -4,4 +4,10 @@ function getColor(){
     let col = document.getElementById('color').value
     document.getElementById('visualizador').style.background = col
     document.getElementById('visualizador').innerHTML = col 
+
+    if (col !== '#ffffff'){
+        document.getElementById('visualizador').style.color = '#fff';
+    } else {
+        document.getElementById('visualizador').style.color = '#000';
+    }
 }
